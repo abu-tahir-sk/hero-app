4e2cf1a6b074ab3b36c81e83838f4d006940b3d5
abu-tahir-sk <abutahir2626@gmail.com>
2026-04-25 00:11:07 -0900

    hero app

---
+++ src/Routes/Root.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../MainLayouts/MainLayout";
+import Home from "../Pages/Home";
+import Apps from "../Pages/Apps";
+import Installation from "../Pages/Installation";
+import NotFount from "../Pages/NotFount";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    
+    children: [
+      { index: true, Component: Home },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      { path: "/installation", Component: Installation },
+     
+    ],
+     
+  },
+  {path: "*", Component: NotFount,}
+]);
+
+export default router;
